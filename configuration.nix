4431abb681993869fb28b7104cb920570349ceb5
AngelPwG <jose.angel.git@gmail.com>
2026-05-31 23:39:11 -0700

    d

--- configuration.nix
+++ configuration.nix
@@ -100,7 +100,7 @@ in
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
-  ]
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
