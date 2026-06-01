e356b4776baf438bda6ffd9b8fc237623482eef7
AngelPwG <jose.angel.git@gmail.com>
2026-05-31 22:46:19 -0700

    fixed: The files were empty

---
+++ dotfiles/sway/random_bg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR_FONDOS="$HOME/Vídeos/Wallpapers"
+
+VIDEO_ELEGIDO=$(find "$DIR_FONDOS" -type f \( -name "*.mp4" -o -name "*.webm" \) | shuf -n 1)
+
+if [ -z "$VIDEO_ELEGIDO" ]; then
+    echo "No se encontraron videos en $DIR_FONDOS"
+    exit 1
+fi
+
+killall -q mpvpaper
+
+mpvpaper -p -o "loop --hwdec=auto no-audio" '*' "$VIDEO_ELEGIDO" &
